184f85f7538923c535e05578da94ed2f8edee3e0
bozo.kopic <bozo@kopic.xyz>
2022-04-11 17:32:08 +0200

    src_py util - get unused tcp/udp port update

--- src_py/hat/util.py
+++ src_py/hat/util.py
@@ -124,15 +124,15 @@ class CallbackRegistry:
                     raise
 
 
-def get_unused_tcp_port() -> int:
+def get_unused_tcp_port(host: str = '127.0.0.1') -> int:
     """Search for unused TCP port"""
     with contextlib.closing(socket.socket()) as sock:
-        sock.bind(('127.0.0.1', 0))
+        sock.bind((host, 0))
         return sock.getsockname()[1]
 
 
-def get_unused_udp_port() -> int:
+def get_unused_udp_port(host: str = '127.0.0.1') -> int:
     """Search for unused UDP port"""
     with contextlib.closing(socket.socket(type=socket.SOCK_DGRAM)) as sock:
-        sock.bind(('127.0.0.1', 0))
+        sock.bind((host, 0))
         return sock.getsockname()[1]
