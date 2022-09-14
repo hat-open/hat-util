7998871c3f414248e8e2dd2b57375afe39b688cf
bozo.kopic <bozo@kopic.xyz>
2022-09-14 16:54:42 +0200

    js fix

--- src_js/index.js
+++ src_js/index.js
@@ -859,14 +859,14 @@ export function delay(fn, t, ...args) {
  * @param {Number} timestamp
  * @return {String}
  */
-function timestampToLocalString(timestamp) {
+export function timestampToLocalString(timestamp) {
     const date = new Date(timestamp * 1000);
-    const YYYY = String(date.getFullYear()).padStart(4, '0')
-    const mm = String(date.getMonth() + 1).padStart(2, '0')
-    const dd = String(date.getDate()).padStart(2, '0')
-    const HH = String(date.getHours()).padStart(2, '0')
-    const MM = String(date.getMinutes()).padStart(2, '0')
-    const SS = String(date.getSeconds()).padStart(2, '0')
-    const fff = String(date.getMilliseconds()).padStart(3, '0')
+    const YYYY = String(date.getFullYear()).padStart(4, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0');
+    const dd = String(date.getDate()).padStart(2, '0');
+    const HH = String(date.getHours()).padStart(2, '0');
+    const MM = String(date.getMinutes()).padStart(2, '0');
+    const SS = String(date.getSeconds()).padStart(2, '0');
+    const fff = String(date.getMilliseconds()).padStart(3, '0');
     return `${YYYY}-${mm}-${dd} ${HH}:${MM}:${SS}.${fff}`;
 }
