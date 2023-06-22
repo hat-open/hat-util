402d46ca69e748b2c5ee69f0cd2f41e95778d48c
bozo.kopic <bozo@kopic.xyz>
2023-06-23 00:38:55 +0200

    py BytesBuffer

--- test_pytest/test_util.py
+++ test_pytest/test_util.py
@@ -116,3 +116,36 @@ def test_get_unused_udp_port():
     port = util.get_unused_udp_port()
     assert isinstance(port, int)
     assert 0 < port <= 0xFFFF
+
+
+def test_bytes_buffer():
+    buff = util.BytesBuffer()
+
+    assert len(buff) == 0
+    data = buff.read()
+    assert bytes(data) == b''
+    assert len(buff) == 0
+
+    buff.add(b'a')
+    buff.add(b'b')
+    buff.add(b'c')
+    assert len(buff) == 3
+    data = buff.read()
+    assert bytes(data) == b'abc'
+    assert len(buff) == 0
+
+    buff.add(b'12')
+    buff.add(b'34')
+    buff.add(b'56')
+    assert len(buff) == 6
+    data = buff.read(3)
+    assert bytes(data) == b'123'
+    assert len(buff) == 3
+    data = buff.read(6)
+    assert bytes(data) == b'456'
+    assert len(buff) == 0
+
+    buff.add(b'123')
+    assert len(buff) == 3
+    assert buff.clear() == 3
+    assert len(buff) == 0
