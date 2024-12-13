b181dc0c995e790d3e9a2d1417790885e4dc4bcc
bozokopic <bozokopic@users.noreply.github.com>
2024-12-13 20:55:06 +0000

    deploy: c3e1e0c80c90a8f642a5c6a86dcd4b41a0375b91

---
+++ c_api/allocator_8h.js
@@ -0,0 +1,9 @@
+var allocator_8h =
+[
+    [ "hat_allocator_t", "structhat__allocator__t.html", "structhat__allocator__t" ],
+    [ "hat_allocator_realloc_t", "allocator_8h.html#a3ce82c0b8d474d7a8b98c2fc2af251d2", null ],
+    [ "hat_allocator_t", "allocator_8h.html#a2598e080a66d81d8b16f203091725a49", null ],
+    [ "hat_allocator_alloc", "allocator_8h.html#ab5074460e3c3a790e29a38d63375c487", null ],
+    [ "hat_allocator_free", "allocator_8h.html#a47cffb8cba5a413ae18370ef84d4255e", null ],
+    [ "hat_allocator_realloc", "allocator_8h.html#ab7722beae314d4da620cedc9e3fa0dcb", null ]
+];
---
+++ c_api/buff_8h.js
@@ -0,0 +1,9 @@
+var buff_8h =
+[
+    [ "hat_buff_t", "structhat__buff__t.html", "structhat__buff__t" ],
+    [ "HAT_BUFF_ERROR", "buff_8h.html#a56ec9474921e973d290afe44e5a0007f", null ],
+    [ "HAT_BUFF_SUCCESS", "buff_8h.html#a5ccd08a35d96d0878342ee6dcc703e09", null ],
+    [ "hat_buff_available", "buff_8h.html#a1b59f66e0ee40cd73e1e7af7959d98a4", null ],
+    [ "hat_buff_read", "buff_8h.html#a0d65c87e2c84cf6b8b93335d4a8b6175", null ],
+    [ "hat_buff_write", "buff_8h.html#af062f8456eeb1f61634611b9f1b760f2", null ]
+];
