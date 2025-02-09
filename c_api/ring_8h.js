5001579ed638b4aff6a4feec4eb20756477b4cd7
bozokopic <bozokopic@users.noreply.github.com>
2025-02-09 20:08:53 +0000

    deploy: 99543dbdec7c47ed780c7a9aa4dc2e91f7322b8a

---
+++ c_api/ring_8h.js
@@ -0,0 +1,14 @@
+var ring_8h =
+[
+    [ "hat_ring_t", "ring_8h.html#ac587ffd5a44b0c616ddeb5c6b9b8df8e", null ],
+    [ "hat_ring_create", "ring_8h.html#a8d7692109e33714d93ef0b6fae916fe4", null ],
+    [ "hat_ring_destroy", "ring_8h.html#a0249f73d9338f69926037cef6e9e7297", null ],
+    [ "hat_ring_len", "ring_8h.html#ac9473014f6b747c40fa37372065f0fa8", null ],
+    [ "hat_ring_move_head", "ring_8h.html#a1d6d7290df6be4aa29ad2e621356c7b2", null ],
+    [ "hat_ring_move_tail", "ring_8h.html#a75545931108b91b896388e3fa15c0e2a", null ],
+    [ "hat_ring_read", "ring_8h.html#ae878650e3e49a9c7516840c0cbd8fb0e", null ],
+    [ "hat_ring_size", "ring_8h.html#a4f7c5ca807c3194f7a1f31273b64879a", null ],
+    [ "hat_ring_unused", "ring_8h.html#a13fc270bc1919fdf2f266bbf588edf56", null ],
+    [ "hat_ring_used", "ring_8h.html#aa46ed3a3619039d4d7836cf9ba2854d1", null ],
+    [ "hat_ring_write", "ring_8h.html#a7586a2e8a92dbbf50965a2f305823d98", null ]
+];
